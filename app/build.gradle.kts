101408c2bd57783c096a94f5408602ae77bf0054
majster0su0lin <majster0su0lin@gmail.com>
2026-08-21 11:47:43 +0200

    Add missing org.jetbrains.kotlin.android plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 import com.google.gms.googleservices.GoogleServicesPlugin.MissingGoogleServicesStrategy
 
 plugins {
+    id("org.jetbrains.kotlin.android")
   alias(libs.plugins.android.application)
   alias(libs.plugins.kotlin.compose)
   alias(libs.plugins.google.devtools.ksp)
